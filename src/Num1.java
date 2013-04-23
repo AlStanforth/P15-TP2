3b999d0a9b35bdfb6d83dc1d83af6fc7a2ffdef1
NullioN <nullion.infinitas@gmail.com>
2013-04-23 12:37:29 -0400

    Added estPremier method in Num1.java

--- src/Num1.java
+++ src/Num1.java
@@ -1,4 +1,21 @@
-
 class Num1 {
-	
+
+    public static void main(String[] args) {
+        System.out.print(estPremier(11));
+        System.out.print(estPremier(12));
+        System.out.print(estPremier(31));
+    }
+
+    public static boolean estPremier(long n) {
+        if (n <= 1) {
+            throw new IllegalArgumentException();
+        }
+        double limite = Math.floor(Math.sqrt(n));
+        for (int i = 2; i <= limite; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
