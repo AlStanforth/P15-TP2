e5ba52ba868324f86bb74cba019b37f782fa2265
Rody Haddad <rody@rodyhaddad.com>
2013-04-25 21:42:32 -0400

    Started Num3

--- src/Num3.java
+++ src/Num3.java
@@ -1,4 +1,8 @@
-
 class Num3 {
 	
+	public long inverseModulo(long a, long m) {
+	   if (a <= 1 || m <= 1 || Num2.euclideEtendu(a, m)[0] != 1) throw new IllegalArgumentException();
+	   
+	   return 0;
+	}
 }
