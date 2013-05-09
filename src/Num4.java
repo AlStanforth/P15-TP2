008841c53c4ebf4cb58bbc176b7e060c6333cde4
Rody Haddad <rody@rodyhaddad.com>
2013-05-09 14:49:45 -0400

    Finished Num4

--- src/Num4.java
+++ src/Num4.java
@@ -1,4 +1,25 @@
 
 class Num4 {
-	
+
+	public static void main(String[] args) {
+		System.out.println(exponentiationRapide(3, 11, 5));
+	}
+
+	public static long exponentiationRapide(long a, long b, long m) {
+		 if (a < 1 || b < 1 || m <= 1) throw new IllegalArgumentException();
+
+		 long produit = 1, puissance = a, quotient = b;
+
+		 do {
+		 	if (quotient % 2 != 0) {
+		 		produit = produit * puissance;
+		 		produit = produit % m;
+		 	}
+		 	puissance = puissance * puissance;
+		 	puissance = puissance % m;
+		 	quotient = quotient / 2;
+		 } while(quotient != 0);
+
+		 return produit;
+	}
 }
