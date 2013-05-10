f15574b9d6689939a0f531193b4afc3341730274
NullioN <nullion.infinitas@gmail.com>
2013-05-10 08:38:26 -0400

    retab Num5 and Num6 to 4-space

--- src/Num6.java
+++ src/Num6.java
@@ -1,27 +1,27 @@
 class Num6 {
 
-	public static long 
-		nProf = Num5.nProf,
-		eProf = Num5.eProf;
+    public static long
+        nProf = Num5.nProf,
+        eProf = Num5.eProf;
 
-	public static String notreMsg = "CETTE PHRASE EST COURTOISE";
-	public static long[]
-		msgEncoded = Outils_A_DISTRIBUER.encoder(notreMsg), 
-		msgCrypter = new long[msgEncoded.length];
+    public static String notreMsg = "CETTE PHRASE EST COURTOISE";
+    public static long[]
+        msgEncoded = Outils_A_DISTRIBUER.encoder(notreMsg),
+        msgCrypter = new long[msgEncoded.length];
 
-	static {
-		for (int i = 0; i < msgEncoded.length; i++) {
-			msgCrypter[i] = Num4.exponentiationRapide(msgEncoded[i], eProf, nProf);
-		}
-	}
+    static {
+        for (int i = 0; i < msgEncoded.length; i++) {
+            msgCrypter[i] = Num4.exponentiationRapide(msgEncoded[i], eProf, nProf);
+        }
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Notre Message : " + notreMsg);
-		System.out.print("Crypter : { " + msgCrypter[0]);
-		for (int i = 1; i < msgCrypter.length; i++) {
-			System.out.print(", " + msgCrypter[i]);
-		}
-		System.out.print(" }\n");
-	}
+    public static void main(String[] args) {
+        System.out.println("Notre Message : " + notreMsg);
+        System.out.print("Crypter : { " + msgCrypter[0]);
+        for (int i = 1; i < msgCrypter.length; i++) {
+            System.out.print(", " + msgCrypter[i]);
+        }
+        System.out.print(" }\n");
+    }
 
 }
